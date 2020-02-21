3eca6eff033f436c7e1bc4e1dc9afbd0f69cc9ca
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-02-21 22:52:32 +0100

    Property Patterns

--- PatternMatching/Program.cs
+++ PatternMatching/Program.cs
@@ -13,13 +13,32 @@ namespace PatternMatching
             {
                 Rectangle(0, 0) => "The value of length and breadth is zero.",
                 Rectangle(10, 5) => "The value of length is 10, breadth is 5.",
-                Rectangle(10, 10) => "The value of length and breadth is same –" +
-        "this represents a square.",
+                Rectangle(10, 10) => "The value of length and breadth is same – this represents a square.",
                 _ => "Meeeh."
             };
 
             Console.WriteLine(result);
+
+            Voyager voyager = new Voyager()
+            {
+                CountryOrigin = "Nigeria",
+                Mileage = 45,
+                Oriki = "Ologo 5G",
+                Destination = "Canada"
+            
+            };
+
+            bool grantEntry = ShouldAllowEntry(voyager);
+            Console.WriteLine("{0}, you have been {1} visa to {2}", voyager.Oriki, grantEntry ? "granted" : "denied", voyager.Destination);
         }
+
+        public static bool ShouldAllowEntry(Voyager hobo) =>
+        hobo switch
+        {
+            { CountryOrigin: "Nigeria", Destination: "Nigeria" } => true,
+            { CountryOrigin: "Nigeria" } => false,
+            _ => false
+        };
     }
 
 
@@ -29,5 +48,13 @@ namespace PatternMatching
         public int Breadth { get; set; }
         public Rectangle(int x, int y) => (Length, Breadth) = (x, y);
         public void Deconstruct(out int x, out int y) => (x, y) = (Length, Breadth);
+    }
+
+    public class Voyager
+    {
+        public string Oriki { get; set; }
+        public decimal Mileage { get; set; }
+        public string CountryOrigin { get; set; }
+        public string Destination { get; set; }
     }
 }
