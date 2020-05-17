78b57c0fcbb383cc642654e3800be49c493ad884
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-05-17 22:59:23 +0100

    Separate Producer & Consumer Threads

---
+++ Channel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelPlayGround
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Channel<string> aChannel = Channel.CreateUnbounded<string>();
+
+            Action ReadFromChannelAction = async () =>
+            {
+                while (await aChannel.Reader.WaitToReadAsync())
+                    Console.WriteLine(await aChannel.Reader.ReadAsync());
+            };
+
+            Action WriteRandomStuffToChannelAction = async () =>
+            {
+                var rnd = new Random();
+                for (short i = 0; i < 5; i++)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(rnd.Next(5)));
+                    await aChannel.Writer.WriteAsync($"Some message from the Law {i}");
+                }
+
+                aChannel.Writer.Complete();
+            };
+
+            var consumer = Task.Run(ReadFromChannelAction);
+
+            var producer = Task.Run(WriteRandomStuffToChannelAction);
+
+            await Task.WhenAll(producer, consumer);
+        }
+    }
+}
